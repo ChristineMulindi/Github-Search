ee8bbdec44d1fa2a1030851f91e17aa1f79c9c34
Christine <you@example.com>
2019-05-06 19:16:27 +0300

    adds API to fix the bug

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
     production: false,
-}
+    apiUrl: "https://api.github.com/users/christinemulindi?access_token=080c24e0209da811bc77c95dc1692cf7cb871de3",
+    apiSearch: 'https://api.github.com/users/daneden?access_token=080c24e0209da811bc77c95dc1692cf7cb871de3'
+
+};
