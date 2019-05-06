b7a7ab45cb3750535c65c8e793c296f482f681c3
Christine <you@example.com>
2019-05-06 19:44:10 +0300

    Auto-generated commit

--- src/app/repository.ts
+++
@@ -1,3 +0,0 @@
-export class Repository {
-    constructor(public name: string, public id: number, public html_url: string, public description: string, public created: Date) {}
-}
--- src/app/user.ts
+++
@@ -1,3 +0,0 @@
-export class User {
-    constructor(public login: string, public id: number, public avatar_url: string, public bio: string){}
-}
